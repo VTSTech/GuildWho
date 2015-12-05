e885d1f7e7aa8ed494048b6620942f42b0a6d8d3
Nigel Todman <veritas@vts-tech.org>
2015-12-04 19:39:41 -0800

    v0.0.8
    Added Kick Reason to GuildWho_Kicked
    Added addkickr sub command
    Added addkick sub command
    Renamed m sub command to addjoin
    Renamed statsp sub command to statsg
    statsg (to /guildchat) now displays Join Date, Rank Date and if it
    exists. Kick Date, Reason and by whom.
    Now properly detects the players own achievements.

--- GuildWho.lua
+++ GuildWho.lua
@@ -3,7 +3,7 @@ GuildWho_Stats = {}
 GuildWho_Kicked = {}
 
 function GUILDWHO_OnLoad()
-  gwhobuild = "v0.0.7";
+  gwhobuild = "v0.0.8";
   this:RegisterEvent("CHAT_MSG_SYSTEM")
 	--arg1    The content of the chat message.
   this:RegisterEvent("CHAT_MSG_GUILD")
@@ -17,10 +17,12 @@ function GUILDWHO_OnLoad()
 	--arg2, arg5    Guildmember Name 
 	--arg11    Chat lineID 
 	--arg12    Sender GUID 			
+	this:RegisterEvent("ACHIEVEMENT_EARNED")
 	--slash commands
 	SlashCmdList["GWHO"] = GUILDWHO_Command;
 	SLASH_GWHO1 = "/guildwho";
 	SLASH_GWHO2 = "/gwho";
+	currPlayer = UnitName("player")
 	print("|cffffcc00GuildWho", gwhobuild,"loaded!");
 end
 
@@ -65,17 +67,19 @@ print("|cffffcc00Local Output:");
 print("|cffffcc00'/guildwho {guild_member}' or '/gwho {guild_member}'");
 print("|cffffcc00'/guildwho stats {guild_member}' or '/gwho stats {guild_member}'");
 print("|cffffcc00Guild Chat Output:");
-print("|cffffcc00'/guildwho statsp {guild_member}' or '/gwho statsp {guild_member}'");
+print("|cffffcc00'/guildwho statsg {guild_member}' or '/gwho statsg {guild_member}'");
 print("|cffffcc00Manual Database submission:");
-print("|cffffcc00'/guildwho m {guild_member} mm/dd/yy mm/dd/yy' or '/gwho m {guild_member} mm/dd/yy mm/dd/yy'");
+print("|cffffcc00'/guildwho addjoin {guild_member} mm/dd/yy mm/dd/yy' or '/gwho addjoin {guild_member} mm/dd/yy mm/dd/yy'");
+print("|cffffcc00'/guildwho addkick {guild_member} mm/dd/yy KickedBy' or '/gwho addkick {guild_member} mm/dd/yy KickedBy'");
+print("|cffffcc00'/guildwho addkickr {guild_member} KickReason' or '/gwho addkick {guild_member} KickReason'");
 end
 
 function GUILDWHO_Command(msg)
    local Cmd, SubCmd = GUILDWHO_GetCmd(msg);
    if (Cmd == "")then
       GUILDWHO_ShowHelp();
-   elseif (strsub(Cmd,1,2) == "m ")then
-      --print("|cffffcc00'(m)anual trigger!");
+   elseif (strsub(Cmd,1,8) == "addjoin ")then
+      --print("|cffffcc00'addjoin trigger!");
       local n=0;
       for token in string.gmatch(Cmd, "[^%s]+") do
          -- print(token)
@@ -97,14 +101,111 @@ function GUILDWHO_Command(msg)
 			tinsert(GuildWho_Saved,getn(GuildWho_Saved),RankDate)   -- rank change date      
 			print("|cffffcc00GuildWho", gwhobuild,"Data stored. use /rl or logout/quit/camp to commit to disk.");
 			end
+	--testing v0.0.8
+  elseif (strsub(Cmd,1,8) == "addkick ")then
+      --print("|cffffcc00'addkick trigger!");
+      local guild, realm = (GetGuildInfo("player")), GetRealmName()
+      local n=0;
+      for token in string.gmatch(Cmd, "[^%s]+") do
+         -- print(token)
+         n=n + 1;
+         if (n == 2)then
+            PlayerName = token;
+         elseif (n == 3)then
+            KickDate = token;
+         elseif (n == 4)then
+            KickedBy = token;
+         end
+      end
+			if (GuildWho_Kicked[realm] == nil) then
+			GuildWho_Kicked[realm] = {}
+			end
+			if (GuildWho_Kicked[realm][guild] == nil) then
+			GuildWho_Kicked[realm][guild] = {}
+			end
+			if(GuildWho_Kicked[realm][guild][PlayerName] == nil) then
+			GuildWho_Kicked[realm][guild][PlayerName] = {}
+			end
+			if(GuildWho_Kicked[realm][guild][PlayerName]["Kick Date"] == nil) then
+			GuildWho_Kicked[realm][guild][PlayerName]["Kick Date"] = {}
+			end
+			if(GuildWho_Kicked[realm][guild][PlayerName]["Kicked By"] == nil) then
+			GuildWho_Kicked[realm][guild][PlayerName]["Kicked By"] = {}
+			end
+			if(GuildWho_Kicked[realm][guild][PlayerName]["Kick Date"]["value"] == nil) then
+			GuildWho_Kicked[realm][guild][PlayerName]["Kick Date"]["value"] = {}
+			end
+			if(GuildWho_Kicked[realm][guild][PlayerName]["Kicked By"]["value"] == nil) then
+			GuildWho_Kicked[realm][guild][PlayerName]["Kicked By"]["value"] = {}
+			end
+      if (GuildWho_Kicked[realm][guild][PlayerName]) then
+      print("|cffffcc00GuildWho", gwhobuild," Player Name:", PlayerName,"is already in the database.");
+      else
+      print("|cffffcc00GuildWho", gwhobuild," Player Name:", PlayerName,"Kick Date: ", KickDate, "Kicked by: ", KickedBy);
+			GuildWho_Kicked[realm][guild][PlayerName]["Kick Date"]["value"] = KickDate
+			GuildWho_Kicked[realm][guild][PlayerName]["Kicked By"]["value"] = KickedBy   
+			print("|cffffcc00GuildWho", gwhobuild,"Data stored. use /rl or logout/quit/camp to commit to disk.");
+			end
+  elseif (strsub(Cmd,1,9) == "addkickr ")then
+      --KickReason isnt specified by the system anywhere, so it will have to be manually added
+      --for each kick. A kick is added and 'Kick Reason' table is not created until this is run.
+      --print("|cffffcc00'addkickr trigger!");
+      local guild, realm = (GetGuildInfo("player")), GetRealmName()
+      local n=0;
+      for token in string.gmatch(Cmd, "[^%s]+") do
+         -- print(token)
+         n=n + 1;
+         if (n == 2)then
+            PlayerName = token;
+         elseif (n == 3)then
+            KickReason = token;
+         elseif (n >= 4)then
+         		KickReason = KickReason .. "." .. token
+         end
+      end
+			if (GuildWho_Kicked[realm] == nil) then
+			GuildWho_Kicked[realm] = {}
+			end
+			if (GuildWho_Kicked[realm][guild] == nil) then
+			GuildWho_Kicked[realm][guild] = {}
+			end
+			if(GuildWho_Kicked[realm][guild][PlayerName] == nil) then
+			GuildWho_Kicked[realm][guild][PlayerName] = {}
+			end
+			if(GuildWho_Kicked[realm][guild][PlayerName]["Kick Date"] == nil) then
+			GuildWho_Kicked[realm][guild][PlayerName]["Kick Date"] = {}
+			end
+			if(GuildWho_Kicked[realm][guild][PlayerName]["Kicked By"] == nil) then
+			GuildWho_Kicked[realm][guild][PlayerName]["Kicked By"] = {}
+			end
+			if(GuildWho_Kicked[realm][guild][PlayerName]["Kick Date"]["value"] == nil) then
+			GuildWho_Kicked[realm][guild][PlayerName]["Kick Date"]["value"] = {}
+			end
+			if(GuildWho_Kicked[realm][guild][PlayerName]["Kicked By"]["value"] == nil) then
+			GuildWho_Kicked[realm][guild][PlayerName]["Kicked By"]["value"] = {}
+			end
+			if(GuildWho_Kicked[realm][guild][PlayerName]["Kick Reason"] == nil) then
+      GuildWho_Kicked[realm][guild][PlayerName]["Kick Reason"] = {}
+    	end
+			if(GuildWho_Kicked[realm][guild][PlayerName]["Kick Reason"]["value"] == nil) then
+      GuildWho_Kicked[realm][guild][PlayerName]["Kick Reason"]["value"] = {}
+    	end
+      if (GuildWho_Kicked[realm][guild][PlayerName]) then
+      	print("|cffffcc00GuildWho", gwhobuild," Player Name:", PlayerName,"is in the GuildWho_Kicked database.");
+      	print("|cffffcc00GuildWho", gwhobuild," Player Name:", PlayerName,"Kick Reason: ", KickReason);
+				GuildWho_Kicked[realm][guild][PlayerName]["Kick Reason"]["value"] = KickReason
+				print("|cffffcc00GuildWho", gwhobuild,"Data stored. use /rl or logout/quit/camp to commit to disk.");
+			else
+				print("|cffffcc00GuildWho", gwhobuild," Player Name:", PlayerName,"is not in the GuildWho_Kicked database!");
+			end
    elseif (strsub(Cmd,1,6) == "stats ")then
       gsPlayerName = strsub(Cmd,7,strlen(Cmd))
       --print("|cffffcc00'stats trigger!", gsPlayerName);
       GUILDWHO_CheckStats(gsPlayerName)
-   elseif (strsub(Cmd,1,7) == "statsp ")then
+   elseif (strsub(Cmd,1,7) == "statsg ")then
       gsPlayerName = strsub(Cmd,8,strlen(Cmd))
-      print("|cffffcc00'statsp trigger!", gsPlayerName);
-      GUILDWHO_CheckStatsP(gsPlayerName)
+      --print("|cffffcc00'statsg trigger!", gsPlayerName);
+      GUILDWHO_CheckStatsG(gsPlayerName)
    else
    GUILDWHO_Lookup(Cmd);
    end
@@ -155,7 +256,7 @@ function GUILDWHO_OnEvent(event)
 	      --print("|cffffcc00GuildWho", gwhobuild,"Debug: (Guild Msg, Database Found): Player Name:", arg2, GuildWho_Stats[realm][guild][arg2]["Chat Lines"]["value"]);
 	      local gchatlines = GuildWho_Stats[realm][guild][arg2]["Chat Lines"]["value"];
 				if (gchatlines == nil or type(gchatlines) == "table") then
-					print("gchatlines is nil or table")
+					--print("gchatlines is nil or table")
 					gchatlines = 0
 				end
 	      local gchatlines = gchatlines + 1
@@ -220,6 +321,56 @@ function GUILDWHO_OnEvent(event)
     	end
     msgguildach = false
     end
+    --testing v0.0.8
+    if(event == "ACHIEVEMENT_EARNED") then
+    	--print("ACHIEVEMENT_EARNED event fired!")
+    	--arg1    The id of the achievement gained. 
+    	msglocal = true
+    	currPlayer = UnitName("player")
+			local gPlayerName = currPlayer
+		  local guild, realm = (GetGuildInfo("player")), GetRealmName()
+			if (GuildWho_Stats[realm] == nil) then
+			GuildWho_Stats[realm] = {}
+			end
+			if (GuildWho_Stats[realm][guild] == nil) then
+			GuildWho_Stats[realm][guild] = {}
+			end
+			if(GuildWho_Stats[realm][guild][gPlayerName] == nil) then
+			GuildWho_Stats[realm][guild][gPlayerName] = {}
+			end
+			if(GuildWho_Stats[realm][guild][gPlayerName]["Chat Lines"] == nil) then
+			GuildWho_Stats[realm][guild][gPlayerName]["Chat Lines"] = {}
+			end
+			if(GuildWho_Stats[realm][guild][gPlayerName]["Achievements"] == nil) then
+			GuildWho_Stats[realm][guild][gPlayerName]["Achievements"] = {}
+			end
+			if(GuildWho_Stats[realm][guild][gPlayerName]["Chat Lines"]["value"] == nil) then
+			GuildWho_Stats[realm][guild][gPlayerName]["Chat Lines"]["value"] = {}
+			end
+			if(GuildWho_Stats[realm][guild][gPlayerName]["Achievements"]["value"] == nil)then
+			GuildWho_Stats[realm][guild][gPlayerName]["Achievements"]["value"] = {}
+			end    
+      if (GuildWho_Stats[realm][guild][gPlayerName]["Achievements"]) then
+	      --print("|cffffcc00GuildWho", gwhobuild,"Debug: (Guild Ach, Database Found): Player Name:", arg2, GuildWho_Stats[realm][guild][arg2]);      
+	      local gachievements = GuildWho_Stats[realm][guild][gPlayerName]["Achievements"]["value"]
+	      if (gachievements == nil or type(gachievements) == "table") then
+	      	--print("gachievements is definately nil or a table")
+	      	gachievements = 0
+	      end
+	      local gachievements = gachievements + 1
+				GuildWho_Stats[realm][guild][gPlayerName]["Achievements"]["value"] = gachievements
+	      --print("|cffffcc00GuildWho", gwhobuild,"Debug: (Guild Ach, just after gchatline increment)", arg2,GuildWho_Stats[realm][guild][gPlayerName],"Chat Lines:",GuildWho_Stats[realm][guild][gPlayerName]["Chat Lines"]["value"]);
+	      --print("|cffffcc00GuildWho", gwhobuild,"Debug: Guild Member: ", gPlayerName, "Chat lines: ", GuildWho_Stats[realm][guild][gPlayerName]["Chat Lines"]["value"], "Achievements seen: ", GuildWho_Stats[realm][guild][gPlayerName]["Achievements"]["value"]);
+      else
+	      GuildWho_Stats[realm][guild][gPlayerName]["Achievements"]["value"] = 0
+	      --print("|cffffcc00GuildWho", gwhobuild,"Debug (Guild Msg, Not Found):", gPlayerName, GuildWho_Stats[realm][guild][gPlayerName]);
+				GuildWho_Stats[realm][guild][gPlayerName]["Chat Lines"]["value"] = 0 -- New entry, 0 chat lines
+				--print("|cffffcc00GuildWho", gwhobuild,"Debug (Guild Ach, New Achievement. 0 Chat Lines):", arg2,GuildWho_Stats[realm][guild][gPlayerName]["Chat Lines"]["value"]);
+				GuildWho_Stats[realm][guild][gPlayerName]["Achievements"]["value"] = 1 -- New entry, 1 achievements
+				--print("|cffffcc00GuildWho", gwhobuild,"Debug (Guild Ach, New Achievement, 1):", arg2,GuildWho_Stats[realm][guild][gPlayerName]["Achievements"]["value"]);
+    	end
+    msglocal = false
+  end
 end
 
 function GUILDWHO_Joined()
@@ -249,12 +400,24 @@ function GUILDWHO_Lookup(PlayerName)
 		print("|cffffcc00Joined:              ", GuildWho_Saved[JDIndex]);
 		print("|cffffcc00Rank Changed: ", GuildWho_Saved[RDIndex]);
 	else
-		print("|cffffcc00GuildWho", gwhobuild," ", PlayerName, " is not in the database.");
+		print("|cffffcc00GuildWho", gwhobuild," ", PlayerName, " is not in the GuildWho_Saved database.");
 	end
 	--print("|cffffcc00GuildWho", gwhobuild,"Debug: GuildWho_Kicked checking...");
 	if (GuildWho_Kicked[realm][guild][PlayerName]) then
 	--print("|cffffcc00GuildWho", gwhobuild,"Debug: GuildWho_Kicked[index-1] = ", GuildWho_Kicked[localindex - 1])
-   	print("|cffff0000Kicked by", GuildWho_Kicked[realm][guild][PlayerName]["Kicked By"]["value"],"on",GuildWho_Kicked[realm][guild][PlayerName]["Kick Date"]["value"]);
+   	--Kick Reason added later, need to init tables.
+			if(GuildWho_Kicked[realm][guild][PlayerName]["Kick Reason"] == nil) then
+      	GuildWho_Kicked[realm][guild][PlayerName]["Kick Reason"] = {}
+    	end
+			if(GuildWho_Kicked[realm][guild][PlayerName]["Kick Reason"]["value"] == nil) then
+      	GuildWho_Kicked[realm][guild][PlayerName]["Kick Reason"]["value"] = {}
+    	end
+    --Kick Reason added later, need to init tables.
+   	if (GuildWho_Kicked[realm][guild][PlayerName]["Kick Reason"]["value"])then
+   		print("|cffff0000Kicked by", GuildWho_Kicked[realm][guild][PlayerName]["Kicked By"]["value"],"on",GuildWho_Kicked[realm][guild][PlayerName]["Kick Date"]["value"],"Kick Reason:",GuildWho_Kicked[realm][guild][PlayerName]["Kick Reason"]["value"]);
+   	else
+   		print("|cffff0000Kicked by", GuildWho_Kicked[realm][guild][PlayerName]["Kicked By"]["value"],"on",GuildWho_Kicked[realm][guild][PlayerName]["Kick Date"]["value"]);
+   	end
 	end
 	msglocal = false
 end
@@ -304,7 +467,13 @@ function GUILDWHO_RankChange()
 			end
 			if(GuildWho_Kicked[realm][guild][gPlayerName]["Kicked By"]["value"] == nil) then
 			GuildWho_Kicked[realm][guild][gPlayerName]["Kicked By"]["value"] = {}
-			end	
+			end
+			if(GuildWho_Kicked[realm][guild][gPlayerName]["Kick Reason"] == nil) then
+      GuildWho_Kicked[realm][guild][gPlayerName]["Kick Reason"] = {}
+    	end
+			if(GuildWho_Kicked[realm][guild][gPlayerName]["Kick Reason"]["value"] == nil) then
+      GuildWho_Kicked[realm][guild][gPlayerName]["Kick Reason"]["value"] = {}
+    	end			
 		 if (GuildWho_Kicked[realm][guild][gPlayerName]) then
 			 print("|cffffcc00GuildWho", gwhobuild,": ", gPlayerName, " is not in the GuildWho_Kicked database. Adding new entry");
 			 GuildWho_Kicked[realm][guild][gPlayerName]["Kick Date"]["value"] = date("%m/%d/%y")
@@ -358,7 +527,7 @@ function GUILDWHO_CheckStats(gsPlayerName)
 	msglocal = false
 end
 
-function GUILDWHO_CheckStatsP(gsPlayerName)
+function GUILDWHO_CheckStatsG(gsPlayerName)
    local guild, realm = (GetGuildInfo("player")), GetRealmName()
    --print("GuildWho", gwhobuild,"Debug: Lookup fired!");
    msglocal = true
@@ -379,10 +548,21 @@ function GUILDWHO_CheckStatsP(gsPlayerName)
          gmsg = gmsg .." Achievements: " .. GuildWho_Stats[realm][guild][gsPlayerName]["Achievements"]["value"]
       end
    end
+		if (tContains(GuildWho_Saved,gsPlayerName) == 1) then
+			JDIndex = localindex + 1
+			RDIndex = localindex + 2
+			gmsg = gmsg .. " Join Date: " .. GuildWho_Saved[JDIndex] .. " Rank Changed: " .. GuildWho_Saved[RDIndex]
+		else
+			print("|cffffcc00GuildWho", gwhobuild," ", PlayerName, " is not in the GuildWho_Saved database.");
+		end
    --print("|cffffcc00GuildWho", gwhobuild,"Debug: GuildWho_Kicked checking...");
    if (GuildWho_Kicked[realm][guild][gsPlayerName]) then
       --print("|cffffcc00GuildWho", gwhobuild,"Debug: GuildWho_Kicked[index-1] = " .. GuildWho_Kicked[localindex - 1])
-         gmsg = gmsg .. " Kicked by " .. GuildWho_Kicked[realm][guild][gsPlayerName]["Kicked By"]["value"] .. " on " .. GuildWho_Kicked[realm][guild][gsPlayerName]["Kick Date"]["value"]
+         if (GuildWho_Kicked[realm][guild][gsPlayerName]["Kick Reason"]["value"])then
+         	gmsg = gmsg .. " Kicked by " .. GuildWho_Kicked[realm][guild][gsPlayerName]["Kicked By"]["value"] .. " on " .. GuildWho_Kicked[realm][guild][gsPlayerName]["Kick Date"]["value"] .. " Kick Reason: " .. GuildWho_Kicked[realm][guild][gsPlayerName]["Kick Reason"]["value"]
+         else
+         	gmsg = gmsg .. " Kicked by " .. GuildWho_Kicked[realm][guild][gsPlayerName]["Kicked By"]["value"] .. " on " .. GuildWho_Kicked[realm][guild][gsPlayerName]["Kick Date"]["value"]
+       	 end
    end
    --print("|cffffcc00GuildWho", gwhobuild,"Debug:",gmsg)
    SendChatMessage(gmsg,"guild")
